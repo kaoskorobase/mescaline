ce1667be41de459f96255cf3ace3845900e05a75
z <z@null2.net>
2008-05-27 17:31:58 +0200

    bip
    darcs-hash:20080527153158-395df-c1ecfd778a8db18b9e36b678124db0e83a45bbcb.gz

--- app/Mescaline.hs
+++ app/Mescaline.hs
@@ -18,12 +18,11 @@ queryFeatureDescIteratee :: (Monad m) => Int -> String -> IterAct m [FeatureDesc
 queryFeatureDescIteratee a b accum = result' (FeatureDescriptor a b:accum)
 
 
-fetchDetails sfid = do
+fetchDetails sfid fid = do
   let
     iter :: (Monad m) => Int -> Int -> Float -> Float -> IterAct m [(Int, Int, Float, Float)]
     iter a b c d  acc = result $ (a, b, c, d):acc
-    bindVals = [bindP (sfid::Int), bindP(2::Int)]
-	--select sf.id, sf.path, u.id, u.onset_time, u.chunk_length, uf.* from source_file sf, unit u, unit_feature uf left join unit on u.sfid=sf.id left join unit_feature on uf.unit_id=u.id where sf.id = ? and uf.feature_id = ?
+    bindVals = [bindP (sfid::Int), bindP(fid::Int)]
     query = prefetch 1000 "select sf.id, u.id, u.onset_time, u.chunk_length, uf.* from source_file sf, unit u, unit_feature uf where sf.id=u.sfid and sf.id = ? and uf.unit_id=u.id and uf.feature_id=? " bindVals
   actual <- doQuery query iter []
   liftIO (print actual)
@@ -37,13 +36,8 @@ test = do
 main :: IO ()
 main = do
     [dbPath] <- getArgs
+	fid <- getLine >>= return . read
     withSession (connect dbPath) ( do
-    -- simple query, returning reversed list of rows.
-    -- select sf.id, sf.path, u.id from source_file sf, unit u left join unit on u.sfid=sf.id;
-    sourceFiles <- doQuery (sql "select * from source_file") querySourceFileIteratee []
-    featureDescs <- doQuery (sql "select * from feature") queryFeatureDescIteratee [] 
-    mapM_ (fetchDetails . SourceFile.id) sourceFiles
-    --liftIO $ putStrLn $ show featureDescs
-    --fetchDetails 5
-	-- bindShortcutExample 
-    )
+		sourceFiles <- doQuery (sql "select * from source_file") querySourceFileIteratee []
+		featureDescs <- doQuery (sql "select * from feature") queryFeatureDescIteratee [] 
+		mapM_ (flip fetchDetails fid . SourceFile.id) sourceFiles)
