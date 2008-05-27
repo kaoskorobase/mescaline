8e265b7b48b99d75ccd7bbffa3649dc5dc19c6a0
z <z@null2.net>
2008-05-27 16:01:20 +0200

    yo, sql correction
    darcs-hash:20080527140120-395df-590894e8552f5a2c86989ed658046751ff18599d.gz

--- app/Mescaline.hs
+++ app/Mescaline.hs
@@ -20,14 +20,19 @@ queryFeatureDescIteratee a b accum = result' (FeatureDescriptor a b:accum)
 
 fetchDetails sfid = do
   let
-    iter :: (Monad m) => Int -> String -> Int -> Float -> Float -> IterAct m [(Int, String, Int, Float, Float)]
-    iter a b c d e acc = result $ (a, b, c, d, e):acc
+    iter :: (Monad m) => Int -> Int -> Float -> Float -> IterAct m [(Int, Int, Float, Float)]
+    iter a b c d  acc = result $ (a, b, c, d):acc
     bindVals = [bindP (sfid::Int), bindP(2::Int)]
-    query = prefetch 1000 "select sf.id, sf.path, u.id, u.onset_time, u.chunk_length, uf.* from source_file sf, unit u, unit_feature uf left join unit on u.sfid=sf.id left join unit_feature on uf.unit_id=u.id where sf.id = ? and uf.feature_id = ?" bindVals
+	--select sf.id, sf.path, u.id, u.onset_time, u.chunk_length, uf.* from source_file sf, unit u, unit_feature uf left join unit on u.sfid=sf.id left join unit_feature on uf.unit_id=u.id where sf.id = ? and uf.feature_id = ?
+    query = prefetch 1000 "select sf.id, u.id, u.onset_time, u.chunk_length, uf.* from source_file sf, unit u, unit_feature uf where sf.id=u.sfid and sf.id = ? and uf.unit_id=u.id and uf.feature_id=? " bindVals
   actual <- doQuery query iter []
   liftIO (print actual)
 
-
+test = do
+	putStrLn "Please enter your name: "
+	name <- getLine
+	liftIO (print name)
+	
 
 main :: IO ()
 main = do
@@ -36,9 +41,9 @@ main = do
     -- simple query, returning reversed list of rows.
     -- select sf.id, sf.path, u.id from source_file sf, unit u left join unit on u.sfid=sf.id;
     sourceFiles <- doQuery (sql "select * from source_file") querySourceFileIteratee []
-    featureDescs <- doQuery (sql "select * from feature") queryFeatureDescIteratee []
+    featureDescs <- doQuery (sql "select * from feature") queryFeatureDescIteratee [] 
     mapM_ (fetchDetails . SourceFile.id) sourceFiles
     --liftIO $ putStrLn $ show featureDescs
-    --otherActions session
+    --fetchDetails 5
 	-- bindShortcutExample 
     )
