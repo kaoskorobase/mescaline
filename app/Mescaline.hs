8ccb6abcd3c3c5d54ed2622665b5a82af26af29e
z <z@null2.net>
2008-06-02 18:29:09 +0200

    more sql stuff
    darcs-hash:20080602162909-395df-5bc0342145e52345c2d2e73a97feabd0ebb5c2a3.gz

--- app/Mescaline.hs
+++ app/Mescaline.hs
@@ -5,11 +5,14 @@ import Control.Monad.Trans
 import Database.Sqlite.Enumerator
 import Database.Enumerator
 import System.Environment
+import Data.Binary
+
 
 import Mescaline.Database.Feature (FeatureDescriptor(..), Feature(..))
 
 import Mescaline.Database.SourceFile (SourceFile(..))
 import qualified Mescaline.Database.SourceFile as SourceFile
+
 
 querySourceFileIteratee :: (Monad m) => Int -> FilePath -> String -> IterAct m [SourceFile]
 querySourceFileIteratee a b c accum = result' (SourceFile a b c:accum)
@@ -19,13 +22,13 @@ queryFeatureDescIteratee a b accum = result' (FeatureDescriptor a b:accum)
 
 
 fetchDetails sfid fid = do
-  let
-    iter :: (Monad m) => Int -> Int -> Float -> Float -> IterAct m [(Int, Int, Float, Float)]
-    iter a b c d  acc = result $ (a, b, c, d):acc
-    bindVals = [bindP (sfid::Int), bindP(fid::Int)]
-    query = prefetch 1000 "select sf.id, u.id, u.onset_time, u.chunk_length, uf.* from source_file sf, unit u, unit_feature uf where sf.id=u.sfid and sf.id = ? and uf.unit_id=u.id and uf.feature_id=? " bindVals
-  actual <- doQuery query iter []
-  liftIO (print actual)
+    let
+        iter :: (Monad m) => Int -> Int -> Float -> Float -> Int -> Float -> Maybe String -> Maybe String -> IterAct m [(Int, Int, Float, Float, Int, Float, Maybe String, Maybe String)]
+        iter a b c d e f g h acc = result $ (a, b, c, d, e, f, g, h):acc
+        bindVals = [bindP (sfid::Int), bindP(fid::Int)]
+        query = prefetch 1000 "select sf.id, u.id, u.onset_time, u.chunk_length, uf.intval, uf.realval, uf.textval, uf.arrayval from source_file sf, unit u, unit_feature uf where sf.id=u.sfid and sf.id = ? and uf.unit_id=u.id and uf.feature_id=? " bindVals
+    actual <- doQuery query iter []
+    liftIO (print actual)
 
 test = do
 	putStrLn "Please enter your name: "
@@ -36,8 +39,9 @@ test = do
 main :: IO ()
 main = do
     [dbPath] <- getArgs
-	fid <- getLine >>= return . read
+    putStrLn "Please enter feature id [1..7]: "
+    fid <- getLine >>= return . read
     withSession (connect dbPath) ( do
-		sourceFiles <- doQuery (sql "select * from source_file") querySourceFileIteratee []
-		featureDescs <- doQuery (sql "select * from feature") queryFeatureDescIteratee [] 
-		mapM_ (flip fetchDetails fid . SourceFile.id) sourceFiles)
+        sourceFiles <- doQuery (sql "select * from source_file") querySourceFileIteratee []
+        featureDescs <- doQuery (sql "select * from feature") queryFeatureDescIteratee [] 
+        mapM_ (flip fetchDetails fid . SourceFile.id) sourceFiles)
