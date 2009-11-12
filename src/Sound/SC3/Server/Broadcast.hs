7e1f17694f8a1645deee78ee74fd22d1eb426476
kersten <kersten@users.sourceforge.net>
2009-11-12 18:51:59 +0100

    Add module comment for Sound.SC3.Server.Broadcast
    To be improved upon ...

--- src/Sound/SC3/Server/Broadcast.hs
+++ src/Sound/SC3/Server/Broadcast.hs
@@ -1,3 +1,22 @@
+-- | Response processing through iteratees.
+--  
+-- The implementation is based on the idea of a Broadcast that broadcasts
+-- messages to any number of registered consumers. The consumers are defined
+-- in terms of iteratees, i.e. they reduce the stream of replies to some
+-- return value when they finally terminate.
+--  
+-- The implementation is based on Control.Concurrent.Chan, which makes it
+-- unnecessary to handle consumer management explicitly: A consumer registers
+-- an IO action that is supposed to initiate some communication with an
+-- external entity and returns an iteratee to "consume" the generated replies.
+-- Internally, the main communication channel is duplicated, so that anything
+-- delivered will be available on the consumer's channel, too. The IO action
+-- is invoked after the channel is duplicated, eliminating a possible race
+-- condition, where the request is sent and the reply is received before the
+-- channel has been duplicated.
+--  
+-- An enumerator is provided for enumerating a Control.Concurrent.Chan, as
+-- well as some iteratees specific to OSC message processing.
 module Sound.SC3.Server.Broadcast (
     Broadcast
   , Consumer
