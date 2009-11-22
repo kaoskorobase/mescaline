65c9fa4cbcc49b9f62da4e8693c53ea0beb5fa0b
kersten <kersten@users.sourceforge.net>
2009-11-22 20:26:39 +0100

    Factor server state independent part of Connection into Sound.OpenSoundControl.Connection

---
+++ src/Sound/OpenSoundControl/Connection.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE ExistentialQuantification #-}
+module Sound.OpenSoundControl.Connection (
+    Connection
+  , new
+  , close
+  , fork
+  , send
+  , Consumer
+  , communicate
+) where
+
+import           Control.Concurrent (ThreadId, forkIO, killThread)
+
+import           Sound.OpenSoundControl (OSC(..), Transport)
+import qualified Sound.OpenSoundControl as OSC
+
+import           Sound.SC3.Server.Broadcast (Broadcast)
+import qualified Sound.SC3.Server.Broadcast as B
+
+data Connection = forall t . Transport t => Connection t (Broadcast OSC) ThreadId
+
+type Consumer a = B.Consumer OSC a
+
+broadcast :: Connection -> Broadcast OSC
+broadcast (Connection _ d _) = d
+
+new :: Transport t => t -> IO Connection
+new t = do
+    d <- B.new
+    r <- forkIO $ recvLoop d
+    return $ Connection t d r
+    where
+        recvLoop d = OSC.recv t >>= return . flatten >>= B.broadcastList d >> recvLoop d
+        flatten m@(Message _ _) = [m]
+        flatten   (Bundle _ xs) = concatMap flatten xs
+
+close :: Connection -> IO ()
+close (Connection t d r) = do
+    killThread r
+    B.close d
+    OSC.close t
+
+fork :: Connection -> (Connection -> IO ()) -> IO ThreadId
+fork c f = forkIO (f c)
+
+send :: Connection -> OSC -> IO ()
+send (Connection t _ _) = OSC.send t
+
+communicate :: Connection -> IO (Consumer a) -> IO a
+communicate conn = B.consume (broadcast conn)
--- src/Sound/SC3/Server/Connection.hs
+++ src/Sound/SC3/Server/Connection.hs
@@ -12,43 +12,35 @@ module Sound.SC3.Server.Connection (
   , unsafeSync
 ) where
 
-import           Control.Concurrent (ThreadId, forkIO, killThread)
+import           Control.Concurrent (ThreadId)
 
 import           Foreign (void)
 
 import           Sound.OpenSoundControl (Datum(..), OSC(..), Transport)
 import qualified Sound.OpenSoundControl as OSC
+import           Sound.OpenSoundControl.Connection (Consumer)
+import qualified Sound.OpenSoundControl.Connection as C
 import           Sound.OpenSoundControl.Time (Time(..))
 
 import           Sound.SC3 (notify)
-import           Sound.SC3.Server.Broadcast (Broadcast)
-import qualified Sound.SC3.Server.Broadcast as B
 import           Sound.SC3.Server.Iteratee as It
 import           Sound.SC3.Server.Notification (done, synced)
 import           Sound.SC3.Server.State (State)
 import qualified Sound.SC3.Server.State as State
 
-data Connection = forall t . Transport t => Connection State t (Broadcast OSC) ThreadId
-
-type Consumer a = B.Consumer OSC a
+data Connection = Connection State C.Connection
 
 state :: Connection -> State
-state (Connection s _ _ _) = s
+state (Connection s _) = s
 
-broadcast :: Connection -> Broadcast OSC
-broadcast (Connection _ _ d _) = d
+conn :: Connection -> C.Connection
+conn (Connection _ c) = c
 
 new :: Transport t => State -> t -> IO Connection
 new s t = do
-    d <- B.new
-    r <- forkIO $ recvLoop d
-    let c = Connection s t d r
+    c <- Connection s `fmap` C.new t
     initServer c
     return c
-    where
-        recvLoop d = OSC.recv t >>= return . flatten >>= B.broadcastList d >> recvLoop d
-        flatten m@(Message _ _) = [m]
-        flatten b@(Bundle _ xs) = concatMap flatten xs
 
 initServer :: Connection -> IO ()
 initServer c = do
@@ -60,24 +52,21 @@ initServer c = do
     return ()
 
 close :: Connection -> IO ()
-close (Connection _ t d r) = do
-    killThread r
-    B.close d
-    OSC.close t
+close = C.close . conn
 
 fork :: Connection -> (Connection -> IO ()) -> IO ThreadId
-fork c f = forkIO (f c)
+fork c f = C.fork (conn c) (f . Connection (state c))
 
 send :: Connection -> OSC -> IO ()
-send (Connection _ t _ _) = OSC.send t
+send = C.send . conn
 
 communicate :: Connection -> IO (Consumer a) -> IO a
-communicate conn = B.consume (broadcast conn)
+communicate = C.communicate . conn
 
 syncWith :: Connection -> (OSC -> OSC) -> IO ()
-syncWith conn f = void $ communicate conn $ do
-    i <- (State.alloc . State.syncId . state) conn
-    send conn (f (Message "/sync" [Int i]))
+syncWith c f = void $ communicate c $ do
+    i <- (State.alloc . State.syncId . state) c
+    send c (f (Message "/sync" [Int i]))
     return $ waitFor (synced i)
 
 appendOSC :: OSC -> OSC -> OSC
@@ -87,9 +76,9 @@ appendOSC   (Bundle t xs)   m@(Message _ _)  = Bundle t        (xs++[m])
 appendOSC   (Bundle t xs1)    (Bundle _ xs2) = Bundle t        (xs1++xs2)
 
 sync :: Connection -> OSC -> IO ()
-sync conn = syncWith conn . appendOSC
+sync c = syncWith c . appendOSC
 
 -- NOTE: This is only guaranteed to work with a transport that preserves
 -- packet order.
 unsafeSync :: Connection -> IO ()
-unsafeSync conn = syncWith conn id
+unsafeSync c = syncWith c id
