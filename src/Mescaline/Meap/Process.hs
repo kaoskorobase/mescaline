20cc7e5e2c4e4f878193927860436dd618293e6f
kersten <kersten@users.sourceforge.net>
2010-03-24 20:55:58 +0100

    Move Meap class files to tools/meap/lib and add local jars to CLASSPATH

--- src/Mescaline/Meap/Process.hs
+++ src/Mescaline/Meap/Process.hs
@@ -6,7 +6,7 @@ module Mescaline.Meap.Process (
 import Data.List                            (intercalate)
 import System.Directory                     (getHomeDirectory, getTemporaryDirectory)
 import System.Exit                          (ExitCode)
-import System.FilePath                      (joinPath)
+import System.FilePath                      (combine, joinPath)
 import System.FilePath.Glob                 (namesMatching)
 import System.IO                            (Handle)
 import System.Process                       (runProcess, waitForProcess)
@@ -15,15 +15,16 @@ import qualified Distribution.Simple.Utils  as Cabal
 java :: String
 java = "java" -- get from config file
 
-getLibraryDirectory :: IO FilePath
-getLibraryDirectory = do
-    home <- getHomeDirectory
-    return (joinPath [home, "lib", "meap-2.0"])
+getLibraryDirectory :: FilePath -> IO FilePath
+getLibraryDirectory dir = do
+    return (joinPath [dir, "lib", "meap-2.0"])
 
 getClassPath :: IO [FilePath]
 getClassPath = do
-    libDir <- getLibraryDirectory
-    namesMatching (joinPath [libDir, "*.jar"])
+    localLibDir <- getLibraryDirectory "tools/meap"
+    homeLibDir <- getHomeDirectory >>= getLibraryDirectory
+    jars <- mapM (namesMatching . flip combine "*.jar") [localLibDir, homeLibDir]
+    return $ concat jars
 
 runMeap :: String -> [String] -> IO ExitCode
 runMeap mainClass args = do
