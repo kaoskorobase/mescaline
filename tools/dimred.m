038cf494aa337f29472972b95071b944ab807395
kersten <kersten@users.sourceforge.net>
2010-04-25 15:29:33 +0200

    Cut relevant columns from feature file

--- tools/dimred.m
+++ tools/dimred.m
@@ -1,5 +1,10 @@
 x = dlmread('spectrum.dat', ' ', 0, 1);
+x = x(:,[(1:13),15]);
 [wd, frac] = pca(x, 2);
-ws = scalem(x*wd, 'domain');
-d = x*wd*ws;
+xr = x*wd;
+ws = scalem(xr, 'domain');
+d = xr*ws;
+scatterd(d);
 dlmwrite('spectrum_pca.dat', d.data, 'delimiter', ' ');
+system('cut -d" " -f1 spectrum.dat | paste -d " " - spectrum_pca.dat > feature.dat');
+% ./dist/build/editfeature/editfeature mescaline.db insert Onset es.globero.mescaline.spectral 2 - < tools/feature.dat
