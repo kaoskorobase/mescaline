5ed5d5ee15202fb6e1cbbe53609564f275db1553
kersten <kersten@users.sourceforge.net>
2010-06-18 15:35:58 +0200

    Remove wait and waitFor in favor of the generic versions in Sound.OpenSoundControl

--- src/Sound/OpenSoundControl/BufferedTransport.hs
+++ src/Sound/OpenSoundControl/BufferedTransport.hs
@@ -9,7 +9,7 @@ module Sound.OpenSoundControl.BufferedTransport (
 ) where
 
 import Control.Concurrent
-import Sound.OpenSoundControl (OSC(..), Transport(..))
+import Sound.OpenSoundControl (OSC(..), Transport(..), waitFor, wait)
 
 data BufferedTransport = forall t . Transport t => BufferedTransport t (Chan OSC)
 
@@ -35,21 +35,3 @@ dup (BufferedTransport t c) = BufferedTransport t `fmap` dupChan c
 -- | Fork a thread with a duplicate transport.
 fork :: BufferedTransport -> (BufferedTransport -> IO ()) -> IO ThreadId
 fork t f = dup t >>= forkIO . f
-
--- | Wait for an OSC message where the supplied function does not give
---   Nothing, discarding intervening messages.
-waitFor :: BufferedTransport -> (OSC -> Maybe a) -> IO a
-waitFor (BufferedTransport _ c) f = loop
-    where
-        loop = do
-            x <- readChan c
-            case f x of
-                Nothing -> loop
-                Just a  -> return a
-
--- | Wait for an OSC message matching a specific address.
-wait :: BufferedTransport -> String -> IO OSC
-wait t s = waitFor t (\osc -> if has_address s osc then Just osc else Nothing)
-    where
-        has_address x (Message y _) = x == y
-        has_address x (Bundle _ xs) = any (has_address x) xs
