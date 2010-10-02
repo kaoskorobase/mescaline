6d28813357feb8eb6291c558cfadaf1c37e48662
kersten <kersten@users.sourceforge.net>
2010-10-02 18:34:48 +0200

    Reimplement implement region movement and resizing
    Instead of using Qt.fItemIsMovable and an itemChange, implement the
    functionality with mouse press, move and release handlers. This allows
    us to use custom modifiers etc.

--- src/Mescaline/Synth/FeatureSpaceView.hs
+++ src/Mescaline/Synth/FeatureSpaceView.hs
@@ -11,12 +11,14 @@ import           Control.Concurrent.Chan
 import           Control.Concurrent.MVar
 import           Control.Monad
 import           Control.Monad.Fix (fix)
+import           Data.Bits
 import qualified Data.IntMap as Map
 import qualified Data.Vector.Generic as V
 import qualified Mescaline.Application as App
 import qualified Mescaline.Database.Unit as Unit
 import qualified Mescaline.Database.Feature as Feature
-import           Mescaline.Synth.FeatureSpace as FSpace
+import           Mescaline.Synth.FeatureSpace (FeatureSpace)
+import qualified Mescaline.Synth.FeatureSpace as FSpace
 import qualified Mescaline.UI as UI
 
 import qualified Qtc.Classes.Gui                    as Qt
@@ -57,25 +59,28 @@ pair v | V.length v >= 2 = (v V.! 0, v V.! 1)
        | V.length v >= 1 = (v V.! 0, 0)
        | otherwise       = (0, 0)
 
+minRadius :: Double
+minRadius = 0.005
+
 -- mouseHandler :: Unit.Unit -> (Unit.Unit -> IO ()) -> Qt.QGraphicsRectItem () -> Qt.QGraphicsSceneMouseEvent () -> IO ()
 -- mouseHandler unit action this evt = action unit >> Qt.mousePressEvent_h this evt
 
-sceneMousePressHandler :: MVar Bool -> Qt.QGraphicsScene () -> Qt.QGraphicsSceneMouseEvent () -> IO ()
-sceneMousePressHandler state view evt = do
-    putStrLn "sceneMousePressHandler"
-    b <- Qt.button evt ()
-    if ((Qt.qEnum_toInt b) == (Qt.qEnum_toInt Qt.eLeftButton))
-        then Qt.ignore evt () >> swapMVar state True >> return ()
-        else Qt.mousePressEvent_h view evt
-
-sceneMouseReleaseHandler :: MVar Bool -> Qt.QGraphicsScene () -> Qt.QGraphicsSceneMouseEvent () -> IO ()
-sceneMouseReleaseHandler state view evt = do
-    putStrLn "sceneMouseReleaseHandler"
-    b <- Qt.button evt ()
-    if ((Qt.qEnum_toInt b) == (Qt.qEnum_toInt Qt.eLeftButton))
-        then swapMVar state False >> return ()
-        else return ()
-    Qt.mouseReleaseEvent_h view evt
+-- sceneMousePressHandler :: MVar Bool -> Qt.QGraphicsScene () -> Qt.QGraphicsSceneMouseEvent () -> IO ()
+-- sceneMousePressHandler state view evt = do
+--     putStrLn "sceneMousePressHandler"
+--     b <- Qt.button evt ()
+--     if ((Qt.qEnum_toInt b) == (Qt.qEnum_toInt Qt.eLeftButton))
+--         then Qt.ignore evt () >> swapMVar state True >> return ()
+--         else Qt.mousePressEvent_h view evt
+-- 
+-- sceneMouseReleaseHandler :: MVar Bool -> Qt.QGraphicsScene () -> Qt.QGraphicsSceneMouseEvent () -> IO ()
+-- sceneMouseReleaseHandler state view evt = do
+--     putStrLn "sceneMouseReleaseHandler"
+--     b <- Qt.button evt ()
+--     if ((Qt.qEnum_toInt b) == (Qt.qEnum_toInt Qt.eLeftButton))
+--         then swapMVar state False >> return ()
+--         else return ()
+--     Qt.mouseReleaseEvent_h view evt
 
 sceneKeyPressEvent :: MVar State -> FeatureSpaceView -> Qt.QKeyEvent () -> IO ()
 sceneKeyPressEvent state view evt = do
@@ -102,48 +107,90 @@ clusterChange regionId onChanged item itemChange value = do
         onChanged (FSpace.updateRegion regionId (\r -> r { FSpace.center = V.fromList [x, y] }))
     return value
 
--- mouseMoveHandler :: Qt.QGraphicsEllipseItem () -> Qt.QGraphicsSceneMouseEvent () -> IO ()
--- mouseMoveHandler item evt = do
---     Qt.mouseMoveEvent_h item evt
---     Qt.IPoint x y <- Qt.scenePos item ()
---     putStrLn $ "mouseMoveEvent " ++ show (x, y)
+regionMousePressHandler :: Region -> (Input -> IO ()) -> Qt.QGraphicsEllipseItem () -> Qt.QGraphicsSceneMouseEvent () -> IO ()
+regionMousePressHandler region onChanged item evt = do
+    mods <- Qt.modifiers evt ()
+    -- Qt.setFlags item $ Qt.fItemIsMovable + (Qt.qFlags_fromInt 2048) -- FIXME: Huh? Wtf? QGraphicsItem::ItemSendsGeometryChanges?
+    -- Qt.unsetFlag item Qt.eItemIsMovable
+    if (Qt.qFlags_toInt mods) .&. (Qt.qFlags_toInt Qt.fControlModifier) /= 0
+        then do
+            Qt.IPoint _ ey <- Qt.scenePos evt ()
+            swapMVar (regionState region) (RegionResize ey)
+            return ()
+        else when (Qt.qFlags_toInt mods == 0) $ do
+            Qt.IPoint ix iy <- Qt.scenePos item ()
+            Qt.IPoint ex ey <- Qt.scenePos evt ()
+            swapMVar (regionState region) (RegionMove (ix-ex) (iy-ey))
+            Qt.accept evt ()
 
-initScene :: FeatureSpaceView -> FeatureSpace -> MVar State -> ((FeatureSpace -> FeatureSpace) -> IO ()) -> (Unit.Unit -> IO ()) -> IO ()
-initScene view model state changed playUnit = do
+regionMouseMoveHandler :: Region -> (Input -> IO ()) -> Qt.QGraphicsEllipseItem () -> Qt.QGraphicsSceneMouseEvent () -> IO ()
+regionMouseMoveHandler region onChanged item evt = do
+    Qt.IPoint x y <- Qt.scenePos evt ()
+    state <- readMVar (regionState region)
+    case state of
+        RegionIdle ->
+            return ()
+        RegionMove dx dy -> do
+            let pos = (x+dx, y+dy)
+            putStrLn $ "Region " ++ show (regionId region) ++ " pos=" ++ show pos
+            onChanged $ Update $ FSpace.updateRegion (regionId region) (\r -> r { FSpace.center = V.fromList [fst pos, snd pos] })
+            Qt.setPos item pos
+            return ()
+        RegionResize y0 -> do
+            Qt.IRect _ _ d _ <- Qt.qboundingRect item ()
+            let dy = y0 - y
+                d' = max (minRadius*2) $ min 1 $ d + dy
+                r' = d'/2
+            putStrLn $ "Region " ++ show (regionId region) ++ " radius=" ++ show r'
+            onChanged $ Update $ FSpace.updateRegion (regionId region) (\r -> r { FSpace.radius = r' })
+            Qt.qsetRect item (Qt.IRect (-r') (-r') d' d')
+            swapMVar (regionState region) (RegionResize y)
+            return ()
+
+regionMouseReleaseHandler :: Region -> (Input -> IO ()) -> Qt.QGraphicsEllipseItem () -> Qt.QGraphicsSceneMouseEvent () -> IO ()
+regionMouseReleaseHandler region onChanged item evt = do
+    swapMVar (regionState region) RegionIdle
+    return ()
+
+initScene :: FeatureSpaceView -> FeatureSpace -> MVar State -> (Input -> IO ()) -> (Unit.Unit -> IO ()) -> IO ()
+initScene view model state onChanged playUnit = do
     -- Qt.setHandler view "mousePressEvent(QGraphicsSceneMouseEvent*)" $ sceneMousePressHandler state
     -- Qt.setHandler view "mouseReleaseEvent(QGraphicsSceneMouseEvent*)" $ sceneMouseReleaseHandler state
     Qt.setSceneRect view (Qt.rectF 0 0 1 1)
     Qt.setHandler view "keyPressEvent(QKeyEvent*)" $ sceneKeyPressEvent state
     Qt.setHandler view "keyReleaseEvent(QKeyEvent*)" $ sceneKeyReleaseEvent state
-    mapM_ mkUnit (units model)
+    mapM_ mkUnit (FSpace.units model)
     colors <- UI.defaultColorsFromFile
     mapM_ (uncurry $ mkRegion state) $ zip (Map.toList (FSpace.regions model)) colors
     where
         mkUnit u = do
-            let (x, y) = pair (Feature.value (feature u))
-                r = 0.005
-                box = Qt.rectF (x-r) (y-r) (r*2) (r*2)
+            let (x, y) = pair (Feature.value (FSpace.feature u))
+                r      = minRadius
+                box    = Qt.rectF (x-r) (y-r) (r*2) (r*2)
             item <- Qt.qGraphicsEllipseItem_nf box
             -- Qt.setHandler item "mousePressEvent(QGraphicsSceneMouseEvent*)" $ mouseHandler (unit u) action
             Qt.setHandler item "hoverEnterEvent(QGraphicsSceneHoverEvent*)" $ hoverHandler $
-                readMVar state >>= flip when (playUnit (unit u)) . playUnits
+                readMVar state >>= flip when (playUnit (FSpace.unit u)) . playUnits
             Qt.setAcceptsHoverEvents item True
             Qt.addItem view item
         mkRegion state (index, region) color = do
-            let r = radius region
-                x = center region V.! 0
-                y = center region V.! 1
+            let r = FSpace.radius region
+                x = FSpace.center region V.! 0
+                y = FSpace.center region V.! 1
             item <- Qt.qGraphicsEllipseItem_nf (Qt.rectF (-r) (-r) (r*2) (r*2))
             Qt.setBrush item =<< Qt.qBrush_nf color
-            Qt.setFlags item $ Qt.fItemIsMovable + (Qt.qFlags_fromInt 2048) -- FIXME: Huh? Wtf? QGraphicsItem::ItemSendsGeometryChanges?
-            -- Qt.setHandler item "mouseMoveEvent(QGraphicsSceneMouseEvent*)" $ mouseMoveHandler
+            -- Qt.setFlags item $ Qt.fItemIsMovable + (Qt.qFlags_fromInt 2048) -- FIXME: Huh? Wtf? QGraphicsItem::ItemSendsGeometryChanges?
             Qt.addItem view item
             Qt.setPos item (Qt.pointF x y)
-            Qt.setHandler item "(QVariant)itemChange(QGraphicsItem::GraphicsItemChange,const QVariant&)" $ clusterChange index changed
-            modifyMVar_ state (\s -> return $ s { regionItems = item : regionItems s })
+            -- Qt.setHandler item "(QVariant)itemChange(QGraphicsItem::GraphicsItemChange,const QVariant&)" $ clusterChange index changed
+            region' <- Region index item `fmap` newMVar RegionIdle
+            Qt.setHandler item "mousePressEvent(QGraphicsSceneMouseEvent*)"   $ regionMousePressHandler   region' onChanged
+            Qt.setHandler item "mouseMoveEvent(QGraphicsSceneMouseEvent*)"    $ regionMouseMoveHandler    region' onChanged
+            Qt.setHandler item "mouseReleaseEvent(QGraphicsSceneMouseEvent*)" $ regionMouseReleaseHandler region' onChanged
+            modifyMVar_ state (\s -> return $ s { regions = region' : regions s })
 
-addRegions :: [Region] -> FeatureSpace -> FeatureSpace
-addRegions regions fspace = foldl (\fs (i, r) -> insertRegion i r fs)
+addRegions :: [FSpace.Region] -> FeatureSpace -> FeatureSpace
+addRegions regions fspace = foldl (\fs (i, r) -> FSpace.insertRegion i r fs)
                           fspace
                           (zip [0..] regions)
 
@@ -153,25 +200,33 @@ inputLoop ichan ochan fspace = do
         Update f -> let fspace' = f fspace
                     in fspace' `seq` inputLoop ichan ochan fspace'
         ActivateRegion (t, i) -> do
-            let (u, fspace') = activateRegion i fspace
-            maybe (return ()) (\u -> writeChan ochan (Activate (t, unit u))) u
+            let (u, fspace') = FSpace.activateRegion i fspace
+            maybe (return ()) (\u -> writeChan ochan (Activate (t, FSpace.unit u))) u
             inputLoop ichan ochan fspace'
         Deactivate _ ->
             inputLoop ichan ochan fspace
 
+data RegionState = RegionIdle | RegionMove Double Double | RegionResize Double
+
+data Region = Region {
+    regionId    :: Int
+  , regionItem  :: Qt.QGraphicsEllipseItem ()
+  , regionState :: MVar RegionState
+  }
+
 data State = State {
-    regionItems :: [Qt.QGraphicsEllipseItem ()]
+    regions :: [Region]
   , playUnits :: Bool
   }
 
 featureSpaceView :: FeatureSpace -> Chan Input -> IO (FeatureSpaceView, Chan Output)
 featureSpaceView fspace0 ichan = do
-    let fspace = addRegions (replicate 4 (Region (V.fromList [0.5, 0.5]) 0.025)) fspace0
+    let fspace = addRegions (replicate 4 (FSpace.Region (V.fromList [0.5, 0.5]) 0.025)) fspace0
     this <- featureSpaceView_
     ochan <- newChan
     state <- newMVar (State [] False)
 
-    initScene this fspace state (writeChan ichan . Update) (writeChan ochan . Activate . (,) (-1))
+    initScene this fspace state (writeChan ichan) (writeChan ochan . Activate . (,) (-1))
 
     forkIO $ inputLoop ichan ochan fspace
     
