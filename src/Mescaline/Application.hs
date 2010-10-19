c4f30231b099a45d5cca1ff96ffec381989f6277
kersten <kersten@users.sourceforge.net>
2010-10-19 19:36:13 +0200

    Make application aware of execution context for data resources
    Currently the three cases handled are:
    
     * Application is run from the source directory
     * Application is run from the Cabal install location (e.g. ~/.cabal/bin)
     * Application is run from a MacOS X application bundle
    
    Each of the three cases requires resources like the .ui file to be
    searched at a different location.

--- src/Mescaline/Application.hs
+++ src/Mescaline/Application.hs
@@ -7,7 +7,9 @@ module Mescaline.Application (
   , buildArch
   , buildPlatform
   , name
+  , version
   , getArgs
+  , getProgramDirectory
   , getResourceDirectory
   , getResourcePath
   , getResourceExecutable
@@ -18,10 +20,12 @@ module Mescaline.Application (
 
 import           Control.Exception
 import           Data.List (isPrefixOf)
+import           Data.Version(Version(..))
 import           Distribution.System(OS(..), Arch(..), Platform(..), buildOS, buildArch, buildPlatform)
+import qualified Paths_mescaline as Paths
 import           Prelude hiding (catch)
 import qualified System.Directory as Dir
-import           System.Environment.FindBin (getProgPath)
+import qualified System.Environment.FindBin as FindBin
 import           System.FilePath
 import qualified System.Environment as Env
 
@@ -29,22 +33,51 @@ import qualified System.Environment as Env
 name :: String
 name = "Mescaline"
 
+-- | The version of this application.
+version :: Version
+version = Paths.version
+
+-- | Cross-platform version of 'System.Environment.getArgs' that filters out
+--   arguments passed by the window system.
 getArgs :: IO [String]
 getArgs =
     if buildOS == OSX
-        -- Get rid of Process Serial Number that is passed to an application bundle binary.
+        -- Get rid of the Process Serial Number that is passed to an application
+        -- bundle binary.
         then filter (not . isPrefixOf "-psn") `fmap` Env.getArgs
         else Env.getArgs
+
+-- | Get the containing directory of the running program.
+getProgramDirectory :: IO FilePath
+getProgramDirectory = FindBin.getProgPath
+
+-- | Get the application's data directory.
+--
+-- When running the installed program (crude check), return the data directory from
+-- the Paths_mescaline module. Otherwise, return @resources@ in the current
+-- directory.
+getDataDirectory :: IO FilePath
+getDataDirectory = do
+    progDir <- getProgramDirectory >>= Dir.canonicalizePath
+    binDir  <- Paths.getBinDir >>= Dir.canonicalizePath
+    if progDir == binDir
+        then Paths.getDataDir
+        else return "resources"
 
 -- | Returns the directory where application resources can be found.
 getResourceDirectory :: IO FilePath
 getResourceDirectory = do
-    p <- getProgPath
     if buildOS == OSX
-        then return $ takeDirectory p </> "Resources"
+        then do
+            -- When running from the .app bundle, return the bundle's resource
+            -- directory, otherwise the default.
+            progDir <- getProgramDirectory
+            if takeFileName progDir == "MacOS"
+                then return $ takeDirectory progDir </> "Resources"
+                else getDataDirectory
         else if buildOS == Linux
-             then return $ takeDirectory p </> "lib" </> name
-             else getUserDataDirectory
+             then getDataDirectory
+             else Dir.getAppUserDataDirectory name
 
 getResourcePath :: FilePath -> IO FilePath
 getResourcePath f = do
@@ -60,7 +93,8 @@ getResourceExecutable f = do
 
 -- | Search an executable, first in the resource directory, then in PATH.
 --
--- The executable can be a path into the resource directory, for searching PATH only the filename component will be used.
+-- The executable can be a path into the resource directory, for searching PATH only
+-- the filename component will be used.
 findExecutable :: FilePath -> IO (Maybe FilePath)
 findExecutable path = do
     resPath <- getResourceExecutable path
@@ -68,16 +102,7 @@ findExecutable path = do
         Nothing -> Dir.findExecutable (takeFileName path)
         _       -> return resPath
 
--- getSystemDataDirectory :: IO FilePath
--- getSystemDataDirectory =
--- #if darwin_HOST_OS == 1
---     return $ "/Library/Application Support" </> name
--- #elif mingw32_HOST_OS == 1
---     getResourceDirectory
--- #else
---     getResourceDirectory
--- #endif
-
+-- | Get the directory where user application data is stored, such as config files.
 getUserDataDirectory :: IO FilePath
 getUserDataDirectory = do
     if buildOS == OSX
@@ -87,6 +112,7 @@ getUserDataDirectory = do
         else
             Dir.getAppUserDataDirectory name
 
+-- | Get a path in the user application data directory.
 getUserDataPath :: FilePath -> IO FilePath
 getUserDataPath f = do
     d <- getUserDataDirectory
