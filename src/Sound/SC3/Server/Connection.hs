0acb3425d09034e751b1e85201c40a1400f5b556
kersten <kersten@users.sourceforge.net>
2010-10-28 16:20:07 +0200

    Use HashTable instead of Map for listeners, implement custom listener id allocator and wrap Send into a newtype

--- src/Sound/SC3/Server/Connection.hs
+++ src/Sound/SC3/Server/Connection.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE ExistentialQuantification
+           , GeneralizedNewtypeDeriving #-}
 module Sound.SC3.Server.Connection (
     Connection
   , state
@@ -14,19 +15,13 @@ module Sound.SC3.Server.Connection (
   , unsafeSync
 ) where
 
-import           Control.Concurrent
-import           Control.Monad
-import           Control.Monad.Writer as Writer
--- import           Data.Iteratee.OSC as It
-import qualified Data.Foldable as Fold
-import qualified Data.Map as Map
-import           Data.Unique
-import           Foreign (void)
-
+import           Control.Concurrent (ThreadId, forkIO)
+import           Control.Concurrent.MVar
+import           Control.Monad.Writer.Strict as Writer
+import qualified Data.HashTable as Hash
 import           Sound.OpenSoundControl (Datum(..), OSC(..), Transport, immediately)
 import qualified Sound.OpenSoundControl as OSC
 import           Sound.OpenSoundControl.Monoid ()
-import           Sound.OpenSoundControl.Time (Time(..))
 
 import           Sound.SC3 (notify)
 import           Sound.SC3.Server.Notification (done, synced)
@@ -35,10 +30,10 @@ import qualified Sound.SC3.Server.State as State
 import           Sound.SC3.Server.State.IO (IOState)
 import qualified Sound.SC3.Server.State.IO as IOState
 
-type ListenerId = Unique
-type Listener = ListenerId -> OSC -> IO ()
-type ListenerMap = Map.Map ListenerId (ListenerId, Listener)
-data Connection = forall t . Transport t => Connection t IOState (MVar ListenerMap)
+type ListenerId  = Int
+type Listener    = OSC -> IO ()
+data ListenerMap = ListenerMap !(Hash.HashTable ListenerId Listener) !ListenerId
+data Connection  = forall t . Transport t => Connection t IOState (MVar ListenerMap)
 
 state :: Connection -> IOState
 state (Connection _ s _) = s
@@ -54,14 +49,15 @@ initServer c = do
 recvLoop :: Connection -> IO ()
 recvLoop c@(Connection t _ _) = do
     osc <- OSC.recv t
-    withMVar (listeners c) $ Fold.mapM_ (\(uid, l) -> l uid osc)
+    withMVar (listeners c) (\(ListenerMap h _) -> mapM_ (\(_, l) -> l osc) =<< Hash.toList h)
     recvLoop c
 
 new :: Transport t => State -> t -> IO Connection
 new s t = do
     ios <- IOState.fromState s
-    ls <- newMVar Map.empty
-    let c = Connection t ios ls
+    h  <- Hash.new (==) Hash.hashInt
+    lm <- newMVar (ListenerMap h 0)
+    let c = Connection t ios lm
     _ <- forkIO $ recvLoop c
     initServer c
     return c
@@ -72,45 +68,47 @@ close (Connection t _ _) = OSC.close t
 fork :: Connection -> (Connection -> IO ()) -> IO ThreadId
 fork c f = forkIO (f c)
 
-type Send a = Writer.Writer OSC a
+newtype Send a = Send { unSend :: Writer.Writer OSC a }
+                    deriving (Functor, Monad, MonadWriter OSC)
 
 send :: OSC -> Send ()
-send = Writer.tell
+send = Send . Writer.tell
 
 addListener :: Listener -> Connection -> IO ListenerId
-addListener l c = do
-    uid <- newUnique
-    modifyMVar_ (listeners c) (return . Map.insert uid (uid, l))
-    return uid
+addListener l c = modifyMVar
+                    (listeners c) $
+                    \(ListenerMap h lid) -> do
+                        Hash.insert h lid l
+                        return (ListenerMap h (lid+1), lid)
 
 removeListener :: ListenerId -> Connection -> IO ()
-removeListener uid c = modifyMVar_ (listeners c) (return . Map.delete uid)
+removeListener uid c = modifyMVar_ (listeners c) (\lm@(ListenerMap h _) -> Hash.delete h uid >> return lm)
 
 async :: Send () -> Connection -> IO ()
 async s (Connection t _ _) = do
-    let osc = Writer.execWriter s
-    when (osc /= mempty) $ OSC.send t osc
+    let osc = Writer.execWriter (unSend s)
+    OSC.send t osc
 
 syncWith :: Send () -> (OSC -> Maybe a) -> Connection -> IO a
 syncWith s f c = do
     res <- newEmptyMVar
-    uid <- addListener (action c res) c
+    uid <- addListener (action res) c
     s `async` c
     a <- takeMVar res
     removeListener uid c
     return a
     where
-        action conn res uid osc = do
+        action res osc = do
             case f osc of
                 Nothing -> return ()
                 Just a  -> putMVar res a
 
 -- | Wait for an OSC message matching a specific address.
 syncAddress :: Send () -> String -> Connection -> IO OSC
-syncAddress s a = s `syncWith` hasAddress a
+syncAddress s a = s `syncWith` hasAddress
     where
-        hasAddress s m@(Message a _) = if s == a then Just m else Nothing
-        hasAddress _ _               = Nothing
+        hasAddress m@(Message a' _) = if a == a' then Just m else Nothing
+        hasAddress _                = Nothing
 
 sync :: Send () -> Connection -> IO ()
 sync s c = do
