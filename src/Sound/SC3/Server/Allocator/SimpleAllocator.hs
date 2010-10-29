096ef20d1213587ca1ccc9a572b082efb02df9cb
kersten <kersten@users.sourceforge.net>
2010-10-29 14:54:52 +0200

    Simplify SimpleAllocator and use BitSet instead of IntSet to avoid space leak
    It seems that the IntSet structure was accumulating unevaluated thunks,
    BitSet should be both stricter and faster.

--- src/Sound/SC3/Server/Allocator/SimpleAllocator.hs
+++ src/Sound/SC3/Server/Allocator/SimpleAllocator.hs
@@ -1,78 +1,60 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE BangPatterns, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses #-}
 module Sound.SC3.Server.Allocator.SimpleAllocator (
     SimpleAllocator
   , cons
 ) where
 
+import Control.Monad.Error
 import Control.DeepSeq (NFData(..))
-import Data.Set as Set
+import Data.BitSet as Set
 import Sound.SC3.Server.Allocator
-import Sound.SC3.Server.Allocator.Range as Range
 
-data SimpleAllocator i = SimpleAllocator {
-    sa_range :: !(Range i)
-  , sa_used  :: !(Set.Set i)
-  , sa_next  :: !i
-  , sa_succ  :: !(i -> i)
-}
-
-instance Show i => Show (SimpleAllocator i) where
-    show a = show (show $ sa_range a, show $ sa_used a, show $ sa_next a)
+data SimpleAllocator i =
+    SimpleAllocator
+        {-# UNPACK #-} !(Range i)
+        {-# UNPACK #-} !(Set.BitSet i)
+        {-# UNPACK #-} !i
+        deriving (Eq, Show)
 
 instance NFData i => NFData (SimpleAllocator i) where
-    rnf (SimpleAllocator x1 x2 x3 x4) =
+    rnf (SimpleAllocator x1 x2 x3) =
         rnf x1 `seq`
-        rnf x2 `seq`
-        rnf x3 `seq`
-            x4 `seq` ()
+            x2 `seq`
+        rnf x3 `seq` ()
 
-cons :: Enum i => Range i -> SimpleAllocator i
-cons r = SimpleAllocator r Set.empty (lowerBound r) succ
+cons :: Range i -> SimpleAllocator i
+cons r = SimpleAllocator r Set.empty (lowerBound r)
 
-usedRange :: (Enum i, Num i, Ord i) => Set.Set i -> Range i
-usedRange s | Set.null s = Range.empty
-            | otherwise  = range (Set.findMin s) (succ (Set.findMax s))
-
-getNext :: Ord i => SimpleAllocator i -> (i, SimpleAllocator i)
-getNext (SimpleAllocator r u n f) = (n, SimpleAllocator r u' n' f)
+findNext :: (Integral i) => SimpleAllocator i -> Maybe i
+findNext (SimpleAllocator r u n) = loop (succ n)
     where
-        n' = f n
-        u' = Set.insert n u
+        loop !i
+            | i == n = Nothing
+            | i == upperBound r = loop (lowerBound r)
+            | Set.member (fromIntegral i) u = loop (succ i)
+            | otherwise = Just i
 
-findInSet :: (Enum i, Ord i, Monad m) => SimpleAllocator i -> i -> m (i, SimpleAllocator i)
-findInSet s@(SimpleAllocator r u _ f) i
-    | not (i `within` r) = fail "SimpleAllocator: No free Ids left"
-    | Set.notMember i u  = return (i, SimpleAllocator r u (succ i) f)
-    | otherwise          = findInSet s (succ i)
+sa_alloc :: (Integral i, MonadError String m) => SimpleAllocator i -> m (i, SimpleAllocator i)
+sa_alloc s@(SimpleAllocator r u n)
+    | Set.member (fromIntegral n) u = throwError "SimpleAllocator: No free Ids left"
+    | otherwise = case findNext s of
+                    Nothing -> throwError "SimpleAllocator: No free Ids left"
+                    Just n' -> return (n, SimpleAllocator r (Set.insert (fromIntegral n) u) n')
 
-findNext :: (Enum i, Ord i, Monad m) => SimpleAllocator i -> Range i -> m (i, SimpleAllocator i)
-findNext s@(SimpleAllocator r u _ _) r' =
-    if not (Range.null ru)
-    then return (getNext (SimpleAllocator r u (lowerBound ru) succ))
-    else if not (Range.null rl)
-         then return (getNext (SimpleAllocator r u (pred (upperBound rl)) pred))
-         else findInSet s (lowerBound r)
-    where
-        rl = range (lowerBound r) (lowerBound r')
-        ru = range (upperBound r') (upperBound r)
+sa_free :: (Integral i, MonadError String m) => i -> SimpleAllocator i -> m (SimpleAllocator i)
+sa_free i (SimpleAllocator r u n) | Set.member (fromIntegral i) u = return (SimpleAllocator r u' n)
+                                  | otherwise = throwError "SimpleAllocator: Id not in use"
+    where u' = Set.delete (fromIntegral i) u
 
-sa_alloc :: (Enum i, Num i, Ord i, Monad m) => SimpleAllocator i -> m (i, SimpleAllocator i)
-sa_alloc s@(SimpleAllocator r u n _) =
-    if n `within` r && not (n `within` r')
-    then return (getNext s)
-    else findNext s r'
-    where r' = usedRange u
+-- Simplest allocator imaginable, for testing.
 
-sa_free :: (Enum i, Num i, Ord i, Monad m) => i -> SimpleAllocator i -> m (SimpleAllocator i)
-sa_free i (SimpleAllocator r u n f) | Set.member i u = return (SimpleAllocator r u' n f)
-                                    | otherwise      = fail "SimpleAllocator: Id not in use"
-    where u' = Set.delete i u
+sa_alloc_ :: (Enum i, Eq i, Monad m) => SimpleAllocator i -> m (i, SimpleAllocator i)
+sa_alloc_ (SimpleAllocator r u n) | n == upperBound r = return (n, SimpleAllocator r u (lowerBound r))
+                                  | otherwise         = return (n, SimpleAllocator r u (succ n)      )
 
--- instance (Bounded i, Integral i) => IdAllocator i (SimpleAllocator i) where
---     alloc (SimpleAllocator i) | i == maxBound = return (i, SimpleAllocator minBound)
---     alloc (SimpleAllocator i)                 = return (i, SimpleAllocator (i + 1))
---     free i a = return a
+sa_free_ :: (Monad m) => i -> SimpleAllocator i -> m (SimpleAllocator i)
+sa_free_ _ = return
 
-instance (Enum i, Num i, Ord i) => IdAllocator i (SimpleAllocator i) where
+instance (Integral i) => IdAllocator i (SimpleAllocator i) where
     alloc = sa_alloc
     free  = sa_free
