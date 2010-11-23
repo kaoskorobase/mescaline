2c9c09501c5c9302d9e9aba5f6ca810d30e0b7fa
kersten <kersten@users.sourceforge.net>
2010-11-23 18:25:16 +0100

    Remove iteratee utilities
    This code is not being used anymore.

--- src/Data/Iteratee/Util.hs
+++
@@ -1,8 +0,0 @@
-module Data.Iteratee.Util where
-
-import Data.Iteratee as I
-import Data.Iteratee.Base.StreamChunk (StreamChunk)
-import Prelude hiding (until)
-
-find :: (StreamChunk s el, Monad m) => (el -> Bool) -> IterateeG s el m el
-find f = I.dropWhile (not.f) >> I.head
