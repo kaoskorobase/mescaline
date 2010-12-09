7257f7eeae1f02e0a35460b21daae92114d099cf
kersten <kersten@users.sourceforge.net>
2010-12-09 16:02:23 +0100

    Remove unused functions

--- src/Mescaline/UI.hs
+++ src/Mescaline/UI.hs
@@ -1,9 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Mescaline.UI (
-    qColorFromRgbaF
-  , colorsFromFile
-  , defaultColorsFromFile
-  , getColor
+  getColor
 ) where
 
 import           Data.Bits
@@ -14,25 +11,6 @@ import           Qtc.ClassTypes.Gui
 import           Qtc.Gui.QColor
 import           Qtc.Classes.Qccs
 import           Text.Regex
-
-qColorFromRgbaF :: (Double, Double, Double, Double) -> IO (QColor ())
-qColorFromRgbaF (r, g, b, a) = qColorFromRgba
-                                ( (shiftL (round (a*255)) 24)
-                              .|. (shiftL (round (r*255)) 16)
-                              .|. (shiftL (round (g*255)) 8)
-                              .|. (shiftL (round (b*255)) 0) )
-
-
-colorsFromFile :: FilePath -> IO [QColor ()]
-colorsFromFile path = do
-    s <- readFile path
-    mapM (f.words) (lines s)
-    where
-        f [cr, cg, cb, ca] = qColorFromRgbaF (read cr, read cg, read cb, read ca)
-        f _                = error "colorsFromFile: parse error"
-
-defaultColorsFromFile :: IO [QColor ()]
-defaultColorsFromFile = colorsFromFile =<< App.getResourcePath "colors.txt"
 
 hexAlphaRegex :: Regex
 hexAlphaRegex = mkRegex "^(#[A-Ba-b0-9][A-Ba-b0-9][A-Ba-b0-9][A-Ba-b0-9][A-Ba-b0-9][A-Ba-b0-9][A-Ba-b0-9][A-Ba-b0-9])([A-Ba-b0-9][A-Ba-b0-9])$"
