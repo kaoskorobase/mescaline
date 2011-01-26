81d9738e78629f406aa487160948f4770da1270f
kersten <kersten@users.sourceforge.net>
2011-01-26 19:01:37 +0100

    Add missing files

---
+++ src/Mescaline/Analysis/Types.hs
@@ -0,0 +1,139 @@
+{-# LANGUAGE DeriveDataTypeable, ScopedTypeVariables #-}
+module Mescaline.Analysis.Types (
+    SoundFile(..)
+  , Unit(..)
+  , getUnit
+  , Descriptor(..)
+  , Feature(..)
+  , getFeature
+  , Analysis(..)
+  , newAnalysis
+  , descriptors
+  , descriptorMap
+  , validate
+  , Analyser(..)
+  , Error
+) where
+
+import           Control.Exception
+import           Control.Monad
+import qualified Control.Monad.Writer as Writer
+-- import           Control.Monad.Error
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lex.Double as Lex
+import qualified Data.Enumerator as E
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import           Data.Typeable (Typeable)
+import qualified Mescaline.Database.Hash as Hash
+import qualified Sound.File.Sndfile as SF
+import qualified Sound.File.Sndfile.Util as SF
+
+data SoundFile = SoundFile {
+    path          :: FilePath
+  , hash          :: Hash.Hash
+  , numChannels   :: Int
+  , sampleRate    :: Double
+  , frames        :: Integer
+  } deriving (Eq, Show)
+
+-- | Create a new SourceFile from a locally available sound file.
+newSoundFile :: FilePath -> IO SoundFile
+newSoundFile path = do
+    info <- SF.getInfo path
+    hash <- Hash.fromFile path
+    return $ SoundFile path hash
+                (SF.channels info)
+                (fromIntegral (SF.samplerate info))
+                (fromIntegral (SF.frames info))
+
+data Unit = Unit {
+    onset        :: Double
+  , duration     :: Double
+  } deriving (Eq, Show)
+
+data Error = ParseError String
+    deriving (Show, Typeable)
+
+instance Exception Error
+
+next :: Monad m => String -> E.Iteratee b m b
+next context = do
+    m <- E.head
+    case m of
+        Nothing -> E.throwError $ ParseError $ "EOF while parsing " ++ context
+        Just x -> return x
+
+getDouble :: Monad m => E.Iteratee B.ByteString m Double
+getDouble = do
+    s <- next "Double"
+    case Lex.readDouble s of
+        Nothing -> E.throwError $ ParseError $ "Error while parsing Double: `" ++ BC.unpack s ++ "'"
+        Just (d, _) -> return d
+
+getUnit :: Monad m => E.Iteratee B.ByteString m Unit
+getUnit = do
+    o <- getDouble
+    d <- getDouble
+    return (Unit o d)
+
+data Descriptor = Descriptor {
+    name :: String
+  , degree :: Int
+  } deriving (Eq, Ord, Show)
+
+data Feature = Feature {
+    descriptor :: Descriptor
+  , value :: [Double]
+  } deriving (Eq, Show)
+
+getFeature :: Monad m => Descriptor -> E.Iteratee B.ByteString m Feature
+getFeature desc = do
+    xs <- replicateM (degree desc) getDouble
+    return (Feature desc xs)
+
+-- type Segmentation = [Unit]
+-- 
+-- fromTimes :: [Double] -> Segmentation
+-- fromTimes [] = []
+-- fromTimes ts@(t0:_) =
+--     if t0 <= 0
+--         then zipWith f (0:tail ts) (tail ts)
+--         else zipWith f (0:ts) ts
+--     where
+--         f t' t = Unit t (t - t')
+
+data Analysis = Analysis {
+    soundFile :: SoundFile
+  , units     :: [(Unit, [Feature])]
+  } deriving (Eq, Show)
+
+newAnalysis :: FilePath -> [(Unit, [Feature])] -> IO Analysis
+newAnalysis path units = liftM (flip Analysis units) (newSoundFile path)
+
+descriptors :: Analysis -> Set.Set Descriptor
+descriptors = Set.fromList . map descriptor . concatMap snd . units
+
+descriptorMap :: Analysis -> Map.Map String Descriptor
+descriptorMap a = Map.fromList (zip (map name ds) ds)
+    where ds = Set.toList (descriptors a)
+
+validate :: Analysis -> [String]
+validate a = Writer.execWriter $ do
+    mapM_ checkFeatures (units a)
+    where
+        checkFeatures (u, fs) = do
+            let n = Set.size (descriptors a)
+                m = length fs
+            when (m /= n) (Writer.tell ["Invalid number of features: " ++ show u ++ " " ++ show fs])
+            mapM_ checkFeature fs
+        checkFeature f =
+            let d = descriptor f
+                l = length (value f)
+            in unless (degree d == l)
+                (Writer.tell ["Invalid feature " ++ show d ++ " -> " ++ show l])
+
+class Analyser a where
+    analyse :: a -> FilePath -> IO Analysis
+    fileExtensions :: a -> [String]
---
+++ src/Mescaline/Database/Vector.hs
@@ -0,0 +1,58 @@
+module Mescaline.Database.Vector (
+    Vector
+  , fromVector
+  , toVector
+  , fromList
+  , toList
+) where
+
+import           Database.Persist.Sqlite
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as B
+import qualified Data.Vector.Generic as V
+import qualified Data.Vector.Storable as SV
+-- import Control.Monad.IO.Class (liftIO)
+import           Database.Persist (PersistField(..))
+import qualified Database.Persist as DB
+import           Database.Persist.Base (PersistValue(..), SqlType(..))
+import qualified Mescaline.Data.ByteString as B
+import qualified Sound.OpenSoundControl.Byte as OSC
+import           Text.Read
+
+newtype Vector = Vector { toVector :: SV.Vector Double } deriving (Eq)
+
+instance Show Vector where
+    show x = "fromList " ++ show (V.toList (toVector x))
+
+instance Read Vector where
+    readsPrec = readPrec_to_S $ do
+        i <- lexP
+        case i of
+            Ident "fromList" -> do
+                xs <- readListPrec
+                return $ Vector (V.fromList xs)
+            _ -> pfail
+
+instance PersistField Vector where
+    toPersistValue (Vector v) = toPersistValue $
+        BS.concat $
+            B.toChunks (OSC.encode_i32 (V.length v))
+         ++ concatMap (B.toChunks.OSC.encode_f64) (V.toList v)
+    fromPersistValue v =
+        case v of
+            PersistByteString bs ->
+                let b = B.fromChunks [bs]
+                    n = OSC.decode_i32 (B.take 4 b)
+                    v = V.fromList (map OSC.decode_f64 (B.segment 8 (B.drop 4 b)))
+                in Right (Vector v)
+            p -> Left ("Couldn't create Vector from PersistValue " ++ show p)
+    sqlType _ = SqlBlob
+
+fromVector :: SV.Vector Double -> Vector
+fromVector = Vector
+
+fromList :: [Double] -> Vector
+fromList = Vector . V.fromList
+
+toList :: Vector -> [Double]
+toList = V.toList . toVector
