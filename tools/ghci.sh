66713d5e67be65deba993a949dc347c1aa6fbb4c
sk <kaoskorobase@gmail.com>
2011-02-23 17:38:15 +0100

    Update package hiding for ghci

--- tools/ghci.sh
+++ tools/ghci.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 args="-XScopedTypeVariables"
-ghci $args -Iinclude -isrc -idist/build/autogen -hide-package transformers -hide-package monads-tf "$@"
+ghci $args -Iinclude -isrc -idist/build/autogen -hide-package monads-fd -hide-package monads-tf "$@"
