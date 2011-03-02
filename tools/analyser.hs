5dea94cf1dbff83ea1ab78ec38f398e8e12c4253
sk <kaoskorobase@gmail.com>
2011-03-02 23:21:20 +0100

    Update to aeson 0.2
    Use the monadic parser interface instead of newtype wrappers.

--- tools/analyser.hs
+++ tools/analyser.hs
@@ -2,7 +2,7 @@
 import           Control.Applicative
 import           Control.Exception as E
 import           Data.Aeson as J
-import qualified Data.Attoparsec as A
+import qualified Data.Attoparsec.Lazy as A
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as B
 import qualified Data.Text as Text
@@ -17,24 +17,23 @@ mkError s = object [ "error" .= Text.pack s ]
 data Params = Params { path :: String }
 
 instance FromJSON Params where
-    fromJSON (Object v) = Params <$> (Text.unpack `fmap` (v .: "path"))
-    fromJSON _ = empty
+    parseJSON (Object v) = Params <$> (v .: "path")
+    parseJSON _ = empty
 
 analyseit b = do
     case A.parse J.json b of
         A.Fail _ _ e -> return $ mkError $ "Invalid input: " ++ e
-        A.Partial _ -> return $ mkError $ "Invalid input: EOF while parsing JSON"
         A.Done _ value -> do
             case fromJSON value of
-                Nothing -> return $ mkError "Invalid input: expected field \"path\""
-                Just params -> E.catch (J.toJSON `fmap` analyse analyser (path params))
-                                $ \(e :: SomeException) -> return $ mkError (show e)
+                J.Error e -> return $ mkError $ "Invalid input: " ++ e
+                J.Success params -> E.catch (J.toJSON `fmap` analyse analyser (path params))
+                                        $ \(e :: SomeException) -> return $ mkError (show e)
 
 main :: IO ()
 main = do
     args <- getArgs
     result <- case args of
-                [] -> BS.getContents >>= analyseit
-                [path] -> BS.readFile path >>= analyseit
+                [] -> B.getContents >>= analyseit
+                [path] -> B.readFile path >>= analyseit
                 _ -> putStrLn "Usage: mescaline-analyser [FILE]" >> exitWith (ExitFailure 1)
     B.putStrLn (J.encode result)
