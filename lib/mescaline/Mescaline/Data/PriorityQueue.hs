ed7f11569478bf19dbdb73daf11e594d11811418
sk <kaoskorobase@gmail.com>
2011-03-25 18:06:00 +0100

    Add PriorityQueue type (newtype wrapper)
    ... so that we can provide a Show instance.

--- lib/mescaline/Mescaline/Data/PriorityQueue.hs
+++ lib/mescaline/Mescaline/Data/PriorityQueue.hs
@@ -1,5 +1,6 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Mescaline.Data.PriorityQueue (
-    PQueue
+    PriorityQueue
   , empty
   , singleton
   , union
@@ -7,6 +8,7 @@ module Mescaline.Data.PriorityQueue (
   , add
   , fromList
   , toList
+  , elems
   , null
   , minView
   , minViewWithKey
@@ -16,18 +18,61 @@ module Mescaline.Data.PriorityQueue (
   , assocs
 ) where
 
-import Data.Foldable (toList)
-import Data.PriorityQueue.FingerTree
+import           Control.Arrow (second)
+import           Data.Foldable (Foldable)
+import qualified Data.Foldable as Fold
+import           Data.Monoid (Monoid)
+import           Data.PriorityQueue.FingerTree (PQueue)
+import qualified Data.PriorityQueue.FingerTree as PQ
 import Prelude hiding (null)
 
-minKey :: Ord k => PQueue k a -> Maybe k
+newtype PriorityQueue k v = PriorityQueue (PQueue k v)
+                            deriving (Foldable, Functor, Monoid)
+
+instance (Ord k, Show k, Show v) => Show (PriorityQueue k v) where
+    show pq = "fromList " ++ show (assocs pq)
+
+empty :: Ord k => PriorityQueue k v
+empty = PriorityQueue PQ.empty
+
+singleton :: Ord k => k -> v -> PriorityQueue k v
+singleton k v = PriorityQueue (PQ.singleton k v)
+
+union :: Ord k => PriorityQueue k v -> PriorityQueue k v -> PriorityQueue k v
+union (PriorityQueue pq1) (PriorityQueue pq2) = PriorityQueue (PQ.union pq1 pq2)
+
+insert :: Ord k => k -> v -> PriorityQueue k v -> PriorityQueue k v
+insert k v (PriorityQueue pq) = PriorityQueue (PQ.insert k v pq)
+
+add :: Ord k => k -> v -> PriorityQueue k v -> PriorityQueue k v
+add k v (PriorityQueue pq) = PriorityQueue (PQ.add k v pq)
+
+fromList :: Ord k => [(k, v)] -> PriorityQueue k v
+fromList = PriorityQueue . PQ.fromList
+
+toList :: Ord k => PriorityQueue k v -> [v]
+toList = Fold.toList
+
+elems :: Ord k => PriorityQueue k v -> [v]
+elems = toList
+
+null :: Ord k => PriorityQueue k v -> Bool
+null (PriorityQueue pq) = PQ.null pq
+
+minView :: Ord k => PriorityQueue k v -> Maybe (v, PriorityQueue k v)
+minView (PriorityQueue pq) = fmap (second PriorityQueue) (PQ.minView pq)
+
+minViewWithKey :: Ord k => PriorityQueue k v -> Maybe ((k, v), PriorityQueue k v)
+minViewWithKey (PriorityQueue pq) = fmap (second PriorityQueue) (PQ.minViewWithKey pq)
+
+minKey :: Ord k => PriorityQueue k v -> Maybe k
 minKey = fmap (fst.fst) . minViewWithKey
 
-minElem :: Ord k => PQueue k a -> Maybe a
+minElem :: Ord k => PriorityQueue k v -> Maybe v
 minElem = fmap (snd.fst) . minViewWithKey
 
 -- | Pop all elements from a priority queue whose key is smaller than or equal to k0.
-split :: Ord k => k -> PQueue k a -> ([(k, a)], PQueue k a)
+split :: Ord k => k -> PriorityQueue k v -> ([(k, v)], PriorityQueue k v)
 split k0 = loop
     where
         -- loop pq as = case minViewWithKey pq of
@@ -37,11 +82,11 @@ split k0 = loop
         --                                       else loop pq' (a:as)
         loop pq = case minViewWithKey pq of
                     Nothing            -> ([], pq)
-                    Just (x@(k, a), pq') -> if k > k0
+                    Just (x@(k, _), pq') -> if k > k0
                                             then ([], pq)
                                             else let (xs, pq'') = loop pq' in (x:xs, pq'')
 
-assocs :: Ord k => PQueue k a -> [(k, a)]
+assocs :: Ord k => PriorityQueue k v -> [(k, v)]
 assocs pq = case minViewWithKey pq of
                 Nothing -> []
-                Just ((k, a), pq') -> (k, a) : assocs pq'
+                Just ((k, v), pq') -> (k, v) : assocs pq'
--- lib/mescaline/tests/Mescaline/Data/PriorityQueue/Test.hs
+++ lib/mescaline/tests/Mescaline/Data/PriorityQueue/Test.hs
@@ -11,16 +11,13 @@ import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck
 
-instance (Ord k, Arbitrary k, Arbitrary v) => Arbitrary (PQueue k v) where
+instance (Ord k, Arbitrary k, Arbitrary v) => Arbitrary (PriorityQueue k v) where
     arbitrary = liftM (fromList) arbitrary
-
-instance (Ord k, Show k, Show v) => Show (PQueue k v) where
-    show pq = "fromList " ++ show (assocs pq)
 
 tests :: [Test]
 tests =
     [ testGroup "properties"
-        [ testProperty "split" $ \(k :: Double) (pq :: PQueue Double Char) ->
+        [ testProperty "split" $ \(k :: Double) (pq :: PriorityQueue Double Char) ->
             let (as, pq') = split k pq in as `mappend` assocs pq' == assocs pq
         ]
     ]
