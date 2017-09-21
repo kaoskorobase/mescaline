5cb9dbd0cefbe550cf466156a076d3bebe51aec2
sk <kaoskorobase@gmail.com>
2017-09-21 23:42:07 +0200

    Use Pattern type from hsc3-lang

--- lib/mescaline-patterns/src/Mescaline/Pattern.hs
+++ lib/mescaline-patterns/src/Mescaline/Pattern.hs
@@ -1,44 +1,14 @@
 module Mescaline.Pattern (
-    -- Pattern
-    -- *Reader
-    ask
-  , local
-  , asks
-    -- *State
-  , get
-  , put
-  , modify
-  , gets
-    -- *MonadRandom
-  , runRand
     -- *Patterns
-  , module Mescaline.Pattern.Ppar
+    module Mescaline.Pattern.Ppar
   , ptrace
-  -- , ptraceEnv
     -- *Base module
-  , module Mescaline.Pattern.Base
+  , module Sound.SC3.Lang.Pattern.P
 ) where
 
-import qualified Control.Monad as M
-import qualified Control.Monad.Random as R
-import           Control.Monad.Reader.Class (MonadReader(..), asks)
-import           Control.Monad.State.Class (MonadState(..), modify, gets)
--- import           Mescaline.Pattern.Environment (Environment)
--- import qualified Mescaline.Pattern.Environment as Env
 import           Mescaline.Pattern.Ppar
-import           Mescaline.Pattern.Base
-import           Debug.Trace
+import           Sound.SC3.Lang.Pattern.P
+import qualified Debug.Trace as Debug
 
--- type Pattern = P Environment
-
-runRand :: R.RandomGen s => P s (R.Rand s a) -> P s a
-runRand = M.join . fmap (\r -> prp $ \s -> let (a, s') = R.runRand r s in (return a, s'))
-
-ptrace :: Show a => String -> P s a -> P s a
-ptrace tag = fmap (\a -> trace (tag ++ show a) a)
-
--- ptraceEnv :: Show a => String -> Pattern a -> Pattern a
--- ptraceEnv tag = punfoldr $ \s p ->
---     case step s p of
---         Done s' -> (s', Nothing)
---         Result s' a p' -> (Env.logMessage (tag ++ show a) s', Just (a, p'))
+ptrace :: Show a => String -> P a -> P a
+ptrace tag = fmap (\a -> Debug.trace (tag ++ show a) a)
--- lib/mescaline-patterns/src/Mescaline/Pattern/Ppar.hs
+++ lib/mescaline-patterns/src/Mescaline/Pattern/Ppar.hs
@@ -1,27 +1,32 @@
 module Mescaline.Pattern.Ppar (
-    ppar
+    ptpar
+  , ppar
 ) where
 
 import           Control.Lens
-import           Data.List (foldl')
+import           Data.List (foldl', unfoldr)
 import qualified Mescaline.Data.PriorityQueue as PQ
-import           Mescaline.Pattern.Base
+import           Sound.SC3.Lang.Pattern.P
 import           Mescaline.Time
 
 -- | Parallel pattern composition
-ppar :: HasDelta a => [P s a] -> P s a
-ppar ps = punfoldr f x0
+ptpar :: HasDelta a => [(Time, P a)] -> P a
+ptpar ps = toP $ unfoldr f x0
     where
-        x0 = (0, foldl' (\pq p -> PQ.insert 0 p pq) PQ.empty ps)
-        f s (t, pq) =
+        x0 = (0, foldl' (\pq (t, p) -> PQ.insert t p pq) PQ.empty ps)
+        f (t, pq) =
             case PQ.minViewWithKey pq of
-                Nothing -> (s, Nothing)
+                Nothing -> Nothing
                 Just ((t', p), pq') ->
-                    case step s p of
-                        Done s' ->
-                            f s' (t, pq')
-                        Result s' a p' ->
-                            let pq'' = PQ.insert (t' + a ^. delta) p' pq'
+                    case unP p of
+                        [] ->
+                            f (t, pq')
+                        (a:as) ->
+                            let p'   = toP as
+                                pq'' = PQ.insert (t' + a ^. delta) p' pq'
                                 dt   = maybe 0 (\t_next -> t_next - t') (PQ.minKey pq'')
                                 a'   = a & delta .~ dt
-                            in (s', Just (a', (t', pq'')))
+                            in Just (a', (t', pq''))
+
+ppar :: HasDelta a => [P a] -> P a
+ppar = ptpar . zip (repeat 0)
