b2950e8a7523e25a7517748c85ae8a89b594f078
sk <kaoskorobase@gmail.com>
2017-09-29 14:47:14 +0200

    Fix indentation

--- lib/mescaline-patterns/src/Mescaline/Pattern/Ppar.hs
+++ lib/mescaline-patterns/src/Mescaline/Pattern/Ppar.hs
@@ -17,21 +17,21 @@ minPrio pq = do
 -- | Parallel pattern composition
 ptpar :: HasDelta a => [(Time, P a)] -> P a
 ptpar ps = toP $ unfoldr f x0
-    where
-        x0 = (0, foldl' (\pq (i, (t, p)) -> PQ.insert i t (unP p) pq)
-                        PQ.empty
-                        (zip [0..] ps))
-        f (t, pq) =
-            case PQ.minView pq of
-                Nothing -> Nothing
-                Just (i, t', p, pq') ->
-                    case p of
-                        [] -> f (t, pq')
-                        (a:p') ->
-                            let pq'' = PQ.insert i (t' + a ^. delta) p' pq'
-                                dt   = maybe 0 (\t_next -> t_next - t') (minPrio pq'')
-                                a'   = a & delta .~ dt
-                            in Just (a', (t', pq''))
+  where
+    x0 = (0, foldl' (\pq (i, (t, p)) -> PQ.insert i t (unP p) pq)
+                    PQ.empty
+                    (zip [0..] ps))
+    f (t, pq) =
+      case PQ.minView pq of
+        Nothing -> Nothing
+        Just (i, t', p, pq') ->
+          case p of
+            [] -> f (t, pq')
+            (a:p') ->
+              let pq'' = PQ.insert i (t' + a ^. delta) p' pq'
+                  dt   = maybe 0 (\t_next -> t_next - t') (minPrio pq'')
+                  a'   = a & delta .~ dt
+              in Just (a', (t', pq''))
 
 ppar :: HasDelta a => [P a] -> P a
 ppar = ptpar . zip (repeat 0)
